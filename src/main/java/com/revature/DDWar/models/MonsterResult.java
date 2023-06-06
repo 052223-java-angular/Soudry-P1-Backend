2d2f3cda697c9b13f5b25a45d5575242ef80b36a
Alexander Soudry <soudryalex@gmail.com>
2023-06-06 16:08:51 -0400

    added models.

---
+++ src/main/java/com/revature/DDWar/models/MonsterResult.java
@@ -0,0 +1,16 @@
+package com.revature.DDWar.models;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class MonsterResult {
+    private String index;
+    private String name;
+    private String url;
+}
