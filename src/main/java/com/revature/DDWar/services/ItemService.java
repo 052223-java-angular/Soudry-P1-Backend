aa92dfb63beb9429c14255d2d776183dc3d39e85
Alexander Soudry <soudryalex@gmail.com>
2023-06-06 16:10:03 -0400

    services for the controller

---
+++ src/main/java/com/revature/DDWar/services/ItemService.java
@@ -0,0 +1,16 @@
+package com.revature.DDWar.services;
+
+// import com.revature.DDWar.repositories.ItemRepository;
+import org.springframework.stereotype.Service;
+// import com.revature.DDWar.entities.Item;
+
+@Service
+public class ItemService {
+    // private final ItemRepository itemRepo;
+
+    public void getItem() {
+
+        // return itemRepo.save(null);
+    }
+    
+}
