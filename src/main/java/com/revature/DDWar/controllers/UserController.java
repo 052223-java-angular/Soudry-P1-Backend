c0ce3cc935840c91bded3fb82ad25556cb2aaf54
Alexander Soudry <soudryalex@gmail.com>
2023-06-21 23:20:44 -0400

    added cors

--- src/main/java/com/revature/DDWar/controllers/UserController.java
+++ src/main/java/com/revature/DDWar/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.revature.DDWar.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
     private final UserService userService;
     private final TokenService tokenService;
